2c8e12bb9a035cc57a804113c5cada319c5fd1e8
ytfcb <143380826+ytfcb@users.noreply.github.com>
2023-08-28 06:21:48 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=ca46960c-afc8-4a9e-8162-e4cdaf83a7b8
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.holehike.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=443
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
